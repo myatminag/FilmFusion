03ee7b9fc0c393b8d58db4866d276e5a302ac08b
MyatMinAung <myatminaung.dev@gmail.com>
2023-06-06 21:06:11 +0630

    :art: setup react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import router from "./router";
+import { useState } from "react";
 
 function App() {
-    return <RouterProvider router={router} />;
+    const [queryClient] = useState(() => new QueryClient());
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    );
 }
 
 export default App;
